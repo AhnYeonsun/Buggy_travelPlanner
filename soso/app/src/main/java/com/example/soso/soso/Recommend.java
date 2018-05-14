bc5b6e54980a3a90bd0f294df02ffc2393013705
LeeSoyun <youn_306@naver.com>
2018-05-14 22:40:33 +0900

    UI v.1.2
    Implementation Checklikst, add_plan, and Recommendation(ONGOING; add spinner, listview).

--- soso/app/src/main/java/com/example/soso/soso/Recommend.java
+++ soso/app/src/main/java/com/example/soso/soso/Recommend.java
@@ -3,16 +3,32 @@ package com.example.soso.soso;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 public class Recommend extends AppCompatActivity {
-    ListView listview1 ;
+    ListView listview1;
+    Button searchBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend);
+        searchBtn=findViewById(R.id.searchBtn);
+        Spinner spinner = findViewById(R.id.regionList);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                searchBtn.setText("선택된 지방: "+parent.getItemAtPosition(position));
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
+            }
+        });
         RecomListViewAdapter adapter;
 
         // Adapter 생성
