bda05390c12ef06056ec5f6b2878fb8c5510d631
Zhaolun <youn_306@naver.com>
2018-05-17 00:14:16 +0900

    UI v2.1
    AddPlan : Check the difference between start date and end date.
    Update application's name and icon.

--- soso/app/src/main/java/com/example/soso/soso/AddPlan.java
+++ soso/app/src/main/java/com/example/soso/soso/AddPlan.java
@@ -129,17 +129,16 @@ public class AddPlan extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Intent intent = new Intent(getApplicationContext(), checklist.class);
-                //startActivity(intent);
+                Intent intent = new Intent(getApplicationContext(), PlanDetail.class);
+                startActivity(intent);
             }
         });
 
         temp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), PlanDetail.class);
-                startActivity(intent);
-                //doDifferent();
+                hi.setText(date_first.getText().toString());
+                doDifferent();
                 //String t = Long.toString(diffDays);
                 //hi.setText(t);
             }
@@ -149,19 +148,35 @@ public class AddPlan extends AppCompatActivity {
 
 
     public void doDifferent() {
-        String s=startDate;
-        String e=endDate;
+        /*String s=date_first.getText().toString();
+        String e=date_last.getText().toString();*/
 
-        try {
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd");
+        String date1 = date_first.getText().toString();
+        String date2 = date_last.getText().toString();
 
-            Date beginDate = formatter.parse(s);
-            Date finishDate = formatter.parse(e);
+        try{ // String Type을 Date Type으로 캐스팅하면서 생기는 예외로 인해 여기서 예외처리 해주지 않으면 컴파일러에서 에러가 발생해서 컴파일을 할 수 없다.
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            // date1, date2 두 날짜를 parse()를 통해 Date형으로 변환.
+            Date FirstDate = format.parse(date1);
+            Date SecondDate = format.parse(date2);
 
-            diff = finishDate.getTime() - beginDate.getTime();
-            diffDays = diff / (24 * 60 * 60 * 1000);
-        } catch (ParseException ex) {
+
+            // Date로 변환된 두 날짜를 계산한 뒤 그 리턴값으로 long type 변수를 초기화 하고 있다.
+            // 연산결과 -950400000. long type 으로 return 된다.
+            long calDate = FirstDate.getTime() - SecondDate.getTime();
+
+            // Date.getTime() 은 해당날짜를 기준으로 1970년 00:00:00 부터 몇 초가 흘렀는지를 반환해준다.
+            // 이제 24*60*60*1000(각 시간값에 따른 차이점) 을 나눠주면 일수가 나온다.
+            long calDateDays = calDate / ( 24*60*60*1000);
+            calDateDays = Math.abs(calDateDays);
+
+            hi.setText(String.valueOf(calDateDays)); //+1ㅎㅐ줘야지 됨
+        }
+        catch(ParseException ex)
+        {
+            // 예외 처리
             ex.printStackTrace();
         }
+
     }
 }
