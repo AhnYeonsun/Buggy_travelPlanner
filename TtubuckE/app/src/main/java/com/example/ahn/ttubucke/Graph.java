50e8310b08adf056ceb5c32de76bec06264182fd
AhnYeonsun <yeonsun0517@gmail.com>
2018-05-24 03:17:01 +0900

    PathAlgo_v4
    return type : object to void

--- TtubuckE/app/src/main/java/com/example/ahn/ttubucke/Graph.java
+++ TtubuckE/app/src/main/java/com/example/ahn/ttubucke/Graph.java
@@ -29,20 +29,26 @@ public class Graph {
                 x2 = ((Vertex) Vertices[j]).vertexX;
                 y2 = ((Vertex) Vertices[j]).vertexY;
                 maps[i][j] = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
-                Log.d("i to j distance : ", String.valueOf(maps[i][j]) + "\n");
+                Log.d(i+" to "+j+" distance : ", String.valueOf(maps[i][j]) + "\n");
             }
         }
-    } //*******
+    }
+
+    private double finalDistance;
+    private int[] finalOrder;
 
     public void run(int v) { //시작 노드 번호
         boolean[] check = new boolean[n];     //해당 노드를 방문했는지 체크할 변수
-        double currentD, minD;                                //총거리
+        double currentD;                                //총거리
+        finalDistance = 0.0;
         int order[] = new int[n];
+        finalOrder = new int[n];
         int count = 0;
 
         //distance값 최대로 초기화.
         for (int i = 0; i < n; i++) {
             check[i] = false;
+            finalOrder[i] = -1;
             order[i] = -1;
         }
 
@@ -52,27 +58,38 @@ public class Graph {
         //minD = Integer.MAX_VALUE;
         order[0] = v; //얘도 함수 시작 시 초기화 해주니까 버릴까?
 
-        DistanceAndOrder fin = Dijkstra(v, currentD, order, check, count); //최종 최단거리와 순서 return됨.
-
-    }
-
-    class DistanceAndOrder {
-        double TotalDistance;
-        int[] order;
-
-        DistanceAndOrder(double d, int[] o) {
-            this.TotalDistance = d;
-            this.order = o;
+//        DistanceAndOrder fin = Dijkstra(v, currentD, order, check, count); //최종 최단거리와 순서 return됨.
+//        System.out.println("Distance : " + fin.getDistance());
+//        for (int i=0;i<n;i++) {
+//            System.out.println("Order : " + fin.order[i]);
+//        }
+        Dijkstra(v,currentD,order,check,count);
+        System.out.println("Final Shortest Path Distance : " + finalDistance);
+        for (int i=0;i<n;i++) {
+            System.out.println("Final Shortest Path Order : " + finalOrder[i]);
         }
     }
 
-    public DistanceAndOrder Dijkstra(int v, double TotalDistance, int[] order, boolean[] check, int count) {
+//    class DistanceAndOrder {
+//        double TotalDistance;
+//        int[] order;
+//
+//        DistanceAndOrder(double d, int[] o) {
+//            this.TotalDistance = d;
+//            this.order = o;
+//        }
+//        double getDistance(){
+//            return TotalDistance;
+//        }
+//    }
+
+    public void Dijkstra(int v, double TotalDistance, int[] order, boolean[] check, int count) {
         double[] distance = new double[n];
         //distance 초기화
         for (int i = 0; i < n; i++) {distance[i] = Integer.MAX_VALUE;}
         distance[v] = 0;
         //return 할 객체 생성
-        DistanceAndOrder dij = new DistanceAndOrder(TotalDistance, order);
+        //DistanceAndOrder dij = new DistanceAndOrder(TotalDistance, order);
 
         check[v] = true; //현재 node true
         order[count] = v; //현재 순서 저장
@@ -84,7 +101,7 @@ public class Graph {
             if (check[i]) cnt++;
         }
         if (cnt == n) { //마지막까지 다 찾았을 때!
-            return dij;
+            return;
         } else { //아닐 때!!
             for (int i = 0; i < n; i++) {
                 //v와 연결된 모든 node 사이의 거리 입력.
@@ -93,11 +110,11 @@ public class Graph {
                     //TotalDistance += maps[v][i]; 아냐.
                     tempD = TotalDistance + maps[v][i];
                     v = i;
-                    return dij = Dijkstra(v, tempD, order, check, count++);
+                    Dijkstra(v, tempD, order, check, count++);
                 }
             }
         }
-        return dij;
+        return;
     }
 }
 
